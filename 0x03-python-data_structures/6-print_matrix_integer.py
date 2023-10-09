0ad301ba844ea8f9d945a46c451faaed87f7d54f
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-10 00:21:14 +0530

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix and (len(matrix) != 1 and matrix[0] is not None):
-        template = "{:d} {:d} {:d}"
-        for i in matrix:
-            new_mat = []
-            for j in i:
-                new_mat.append(j)
-            num = template.format(*new_mat)
-            print(num)
+        for row in matrix:
+            for element in row:
+                print("{:d}".format(element), end=' ')
+            print()  # Move to the next line for the next row
     else:
         return None
