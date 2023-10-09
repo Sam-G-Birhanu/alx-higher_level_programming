2ac766b0cde6b95de52b15973fc110b1cf89058b
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-09 05:13:10 -0700

    my commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    template = "{:d}"
-    idx = len(my_list) - 1
-    for n in range(len(my_list)):
-        n = my_list[idx]
-        num = template.format(n)
-        print(num)
-        idx -= 1
+    if my_list:
+        template = "{:d}"
+        idx = len(my_list) - 1
+        for n in range(len(my_list)):
+            n = my_list[idx]
+            num = template.format(n)
+            print(num)
+            idx -= 1
