c09c7e42a5244906aee62ab60c08cbf01120c37e
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-10 02:25:10 -0700

    created functions

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add(a, b):
+    return (a + b )
+if __name__ == "__main__":
+    import sys
+    result = add(int(sys.argv[1]),int(sys.argv[2]))
+    print("{:d}".format(result))
