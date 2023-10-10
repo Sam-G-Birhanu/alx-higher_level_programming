17a7ce70e53f6a7399c25f134017156c82d9faf8
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-11 00:26:49 +0530

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    sum_result = a + b
-    return sum_result
-
-
 if __name__ == "__main__":
-    import sys
-    arg1 = int(sys.argv[1])
-    arg2 = int(sys.argv[2])
-    result = add(arg1, arg2)
-    print("{:d}".format(result))
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
