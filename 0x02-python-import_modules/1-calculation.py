53317b6ffa8e2c4542c8e5e8b8865860f7f89f69
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-11 03:37:52 -0700

    created functions

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "main":
+    a = 10
+    b = 5
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
