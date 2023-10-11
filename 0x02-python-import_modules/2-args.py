e3e8dbc5483a12e1ef874f2b9fa41026a944f5bf
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-11 05:42:15 -0700

    added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     total = 0
     if len(argv) == 1:
         total = 0
+        print("{:d} arguments. ".format(total))
     else:
         total = num_args - 1
-    print("{:d} arguments: ".format(total))
-    for idx in range(1, num_args):
-        print("{:d} : {:} ".format(idx, argv[idx]))
+        print("{:d} arguments: ".format(total))
+        for idx in range(1, num_args):
+            print("{:d} : {:} ".format(idx, argv[idx]))
