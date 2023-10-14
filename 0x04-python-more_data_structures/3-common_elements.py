e688265ab0a82f21ed53f28a489c969897ff9e87
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-13 22:50:38 -0700

    added

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def common_elements(set_1, set_2):
+    com_set = set()
+    for item_1 in set_1:
+        for item_2 in set_2:
+            if item_1 == item_2:
+                com_set.add(item_1)
+    return com_set
