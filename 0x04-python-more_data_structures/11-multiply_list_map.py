355aa2dadfac53bab4ad6b8701f969189e42071c
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-14 05:15:16 -0700

    added

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    def smallf(num):
-        return (num * number)
-    mul = map(smallf, my_list)
-    new = list(mul)
-    return new
+    return list(map(lambda x: x*number, my_list))
