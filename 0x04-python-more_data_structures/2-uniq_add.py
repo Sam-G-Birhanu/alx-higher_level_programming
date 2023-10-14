7eba01188c34653dc9683ba4d720dd57d7e62a57
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-13 22:32:29 -0700

    created

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new = []
+    total = 0
+    for x in my_list:
+        if x not in new:
+            new.append(x)
+    for num in new:
+        total += num
+    return total
