e99ee7553951e0859d264b58c9f7eb852dcaa873
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-14 04:26:09 -0700

    added

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_num = 0
+    f_val = ""
+    if a_dictionary is None:
+        print(None)
+        return None
+    else:
+        for val, num in a_dictionary.items():
+            if num > max_num:
+                max_num = num
+                f_val = val
+    print(f_val)
+    return f_val
