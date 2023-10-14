448a9cc4a296f018e003991a5f674f778ca26162
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-14 04:28:57 -0700

    added

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,12 +3,12 @@ def best_score(a_dictionary):
     max_num = 0
     f_val = ""
     if a_dictionary is None:
-        print(None)
+        print("Best score: {}".format(None))
         return None
     else:
         for val, num in a_dictionary.items():
             if num > max_num:
                 max_num = num
                 f_val = val
-    print(f_val)
+    print("Best score: {}".format(f_val))
     return f_val
