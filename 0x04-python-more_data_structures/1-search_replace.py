2e041e8358fe6ef1ca9757d910c80f7e27028180
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-13 22:13:02 -0700

    added

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new = []
+    for x in my_list:
+        if x != search:
+            new.append(x)
+        else:
+            x = replace
+            new.append(x)
+    return new
