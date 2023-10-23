8458de3c0aec38c2b1d028b14a7a95a28dac7312
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-23 14:59:37 +0530

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print("{}".format(str(i) + str(j)), end="")
-        if int(str(i) + str(j)) < 89:
-            print(", ", end="")
-print("")
+def islower(c):
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    if c in lower:
+        return True
+    else:
+        return False
