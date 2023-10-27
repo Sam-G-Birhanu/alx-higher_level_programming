08dbeda83fd446d8324f54c259d788cba246eb97
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-27 11:56:20 +0530

    Update 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+def add_integer(a, b=98):
+    if a:
+        if (type(a) == int or type(a) == float) and (type(b) == int or type(b) == float):
+            if type(a) == float:
+                a = int(a)
+            if type(b) == float:
+                b = int(b)
+            return a + b
+        else:
+            try:
+                if (type(a) != int and type(a) != float):
+                    print(a, end=" ")
+                    raise Exception
+                if (type(b) != int and type(b) != float):
+                    print(b, end=" ")
+                    raise Exception
+            except Exception:
+                print("must be an integer")
