955c2c97798e8d99bc4eae6bb45c0a452986f81a
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-12-27 22:58:51 -0800

    fixed pycodesyle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" This is a module containing a class that defines an empty rectangle """
 
-""" Rectangle is a class that defines an empty rectangle """
 
 class Rectangle:
+    """ Rectangle s a class name defining a rectangle """
+
     pass
