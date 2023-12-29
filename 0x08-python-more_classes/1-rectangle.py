50e2a16ec0f608c447da79a54a323bdd7f98599b
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-12-29 14:34:24 +0530

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
 """ this module contains a class named Rectangle """
+class Rectangle:
+    """ this class defines a rectangle """
+    def __init__(self , width = 0, height = 0):
+        self._width = width
+        self._height = height
+        
+    @property
+    def width(self):
+        return self._width
+        
+    @width.setter
+    def width(self , value):
+        self._width = value
+        
+    @property
+    def height(self):
+        return self._height
+        
+    @height.setter
+    def height(self , value):
+        self._height = value
 
 
